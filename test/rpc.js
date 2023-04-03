8de4dd5fd476f7ad30eb5c4d7ca847e247d64bd5
mintthemoon <mint@mintthemoon.xyz>
2023-04-03 16:36:18 -0400

    refactor test suites

--- test/rpc.js
+++ test/rpc.js
@@ -4,50 +4,42 @@ const { ProposalStatus } = require('cosmjs-types/cosmos/gov/v1beta1/gov')
 const expect = require('chai').expect
 const k = require('kujira')
 
-describe('rpc health', function() {
-    k.RPCS['kaiyo-1'].map(rpc => 
-        it(`${rpc} health`, () => fetch(`${rpc}/health`).then(res => {
-            expect(res.status).to.eq(200)
-        }))
-    )
-})
-
-describe('rpc order books', function() {
+describe('rpc test suite', function() {
     k.RPCS['kaiyo-1'].map(rpc => {
-        it(`${rpc} axlUSDC/USK book`, () => {
-            market_addr = 'kujira1rwx6w02alc4kaz7xpyg3rlxpjl4g63x5jq292mkxgg65zqpn5llq202vh5'
-            market = CosmWasmClient.connect(rpc).then(client => new k.fin.FinQueryClient(client, market_addr))
-            return market.then(m => m.book({}).then(book => {
-                expect(book).to.have.property('base')
-                expect(book).to.have.property('quote')
+        describe(rpc, function() {
+            it('health', () => fetch(`${rpc}/health`).then(res => {
+                expect(res.status).to.eq(200)
             }))
-        })
-        it(`${rpc} KUJI/axlUSDC book`, () => {
-            market_addr = 'kujira14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sl4e867'
-            market = CosmWasmClient.connect(rpc).then(client => new k.fin.FinQueryClient(client, market_addr))
-            return market.then(m => m.book({}).then(book => {
-                expect(book).to.have.property('base')
-                expect(book).to.have.property('quote')
-            }))
+            it(`axlUSDC/USK book`, () => {
+                market_addr = 'kujira1rwx6w02alc4kaz7xpyg3rlxpjl4g63x5jq292mkxgg65zqpn5llq202vh5'
+                market = CosmWasmClient.connect(rpc).then(client => new k.fin.FinQueryClient(client, market_addr))
+                return market.then(m => m.book({}).then(book => {
+                    expect(book).to.have.property('base')
+                    expect(book).to.have.property('quote')
+                }))
+            })
+            it(`KUJI/axlUSDC book`, () => {
+                market_addr = 'kujira14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sl4e867'
+                market = CosmWasmClient.connect(rpc).then(client => new k.fin.FinQueryClient(client, market_addr))
+                return market.then(m => m.book({}).then(book => {
+                    expect(book).to.have.property('base')
+                    expect(book).to.have.property('quote')
+                }))
+            })
+            it(`gov proposals`, () => Tendermint34Client.connect(rpc)
+                .then(client => k.kujiraQueryClient({client}))
+                .then(kujira => kujira.gov.proposals(ProposalStatus.PROPOSAL_STATUS_UNSPECIFIED, "", "", {
+                    key: new Uint8Array(),
+                    offset: 0,
+                    limit: 5,
+                    countTotal: true,
+                    reverse: true
+                }))
+                .then(res => {
+                    expect(res).to.have.property('proposals')
+                    expect(res.proposals).to.have.lengthOf(5)
+                })
+            )
         })
     })
-})
-
-describe('rpc gov proposals', function() {
-    k.RPCS['kaiyo-1'].map(rpc => 
-        it(`${rpc} gov proposals`, () => Tendermint34Client.connect(rpc)
-            .then(client => k.kujiraQueryClient({client}))
-            .then(kujira => kujira.gov.proposals(ProposalStatus.PROPOSAL_STATUS_UNSPECIFIED, "", "", {
-                key: new Uint8Array(),
-                offset: 0,
-                limit: 5,
-                countTotal: true,
-                reverse: true
-            }))
-            .then(res => {
-                expect(res).to.have.property('proposals')
-                expect(res.proposals).to.have.lengthOf(5)
-            })
-        )
-    )
 })
